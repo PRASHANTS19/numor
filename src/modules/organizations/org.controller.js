7eaab09fc78f01cfb7729f1953b980c6ecdb63e5
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2026-03-11 22:44:09 +0530

    added org logo apis

--- src/modules/organizations/org.controller.js
+++ src/modules/organizations/org.controller.js
@@ -22,7 +22,41 @@ async function updateMyOrganization(req, res) {
   });
 }
 
+async function uploadLogo(req, res, next) {
+  try {
+    const user = req.user;
+    const file = req.file;
+    const logoUrl = await orgService.uploadLogo(user, file);
+    res.json({ success: true, logoUrl });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+async function getLogo(req, res, next) {
+  try {
+    const user = req.user;
+    const logoUrl = await orgService.getLogo(user);
+    res.json({ success: true, logoUrl });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+async function deleteLogo(req, res, next) {
+  try {
+    const user = req.user;
+    await orgService.deleteLogo(user);
+    res.json({ success: true, message: "Logo deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
 module.exports = {
   getMyOrganization,
   updateMyOrganization,
+  uploadLogo,
+  getLogo,
+  deleteLogo
 };
--- src/modules/organizations/org.service.js
+++ src/modules/organizations/org.service.js
@@ -1,12 +1,13 @@
 const prisma = require("../../config/database");
+const storageService = require('../../storage/storage.service');
 
-function getById(orgId) {
+async function getById(orgId) {
   return prisma.organization.findUnique({
     where: { id: BigInt(orgId) },
   });
 }
 
-function update(orgId, data) {
+async function update(orgId, data) {
   return prisma.organization.update({
     where: { id: BigInt(orgId) },
     data: {
@@ -28,7 +29,80 @@ function update(orgId, data) {
   });
 }
 
+async function uploadLogo(user, file) {
+  const existing = await prisma.organization.findUnique({
+    where: { id: BigInt(user.orgId) },
+    select: { logoUrl: true }
+  });
+
+  if (existing?.logoUrl) {
+    await storageService.remove(existing.logoUrl);
+  }
+  const allowedImageTypes = [
+    "image/png",
+    "image/jpeg",
+    "image/jpg",
+    "image/webp",
+    "image/jfif"
+  ];
+
+  if (!allowedImageTypes.includes(file.mimetype)) {
+    throw new Error("Profile photo must be PNG, JPG, JPEG, WEBP or JFIF");
+  }
+
+  fileKey = `organization/org-logo/${user.orgId}/${Date.now()}-${file.originalname}`;
+  const fileBuffer = file.buffer;
+
+  await storageService.upload(fileKey, fileBuffer, file.mimetype);
+  await prisma.organization.update({
+    where: { id: user.orgId },
+    data: {
+      logoUrl: fileKey
+    }
+  });
+    // generate signed URL
+  const signedUrl = await storageService.getSignedUrl(fileKey);
+
+  return signedUrl;
+
+};
+
+async function getLogo(user) {
+
+  const dbUser = await prisma.organization.findUnique({
+    where: { id: BigInt(user.orgId) },
+    select: { logoUrl: true }
+  });
+
+  if (!dbUser || !dbUser.logoUrl) {
+    return { logoUrl: null };
+  }
+
+  const url = await storageService.getSignedUrl(dbUser.logoUrl);
+
+  return url;
+};
+
+async function deleteLogo(user) {
+
+  const dbUser = await prisma.organization.findUnique({
+    where: { id: BigInt(user.orgId) },
+    select: { logoUrl: true }
+  });
+
+  if (dbUser && dbUser.logoUrl) {
+    await storageService.remove(dbUser.logoUrl);
+    await prisma.organization.update({
+      where: { id: BigInt(user.orgId) },
+      data: { logoUrl: null }
+    });
+  }
+}
+
 module.exports = {
   getById,
   update,
+  uploadLogo,
+  getLogo,
+  deleteLogo
 };

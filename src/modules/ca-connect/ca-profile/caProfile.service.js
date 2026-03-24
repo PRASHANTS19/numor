0fe1c0ae4b6cc9833dff75a310818f41ee150255
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2026-03-24 21:34:19 +0530

    fix delete doc

--- src/modules/ca-connect/ca-profile/caProfile.service.js
+++ src/modules/ca-connect/ca-profile/caProfile.service.js
@@ -480,7 +480,7 @@ exports.deleteDocument = async (user, documentId) => {
       return {
         message: "Pending document removed successfully"
       };
-    }
+    } 
 
     if (pendingDoc.operation === "DELETE") {
       throw new Error("Delete already requested");
@@ -492,7 +492,8 @@ exports.deleteDocument = async (user, documentId) => {
   // =========================================================
   if (
     caProfile.status === "PENDING" ||
-    caProfile.status === "UNDER_REVIEW"
+    caProfile.status === "UNDER_REVIEW" ||
+    caProfile.status === "REJECTED"
   ) {
     await storageService.remove(document.fileKey);
 
@@ -505,23 +506,6 @@ exports.deleteDocument = async (user, documentId) => {
     });
 
     return { message: "Document deleted successfully" };
-  }
-
-  // =========================================================
-  // ✅ CASE C: REJECTED → Main flow
-  // =========================================================
-    if (caProfile.status === "REJECTED") {
-    await storageService.remove(document.fileKey);
-
-    await prisma.cADocument.delete({
-      where: { id: document.id }
-    });
-    await prisma.cAProfile.update({
-      where: { userId: user.userId },
-      data: { status: "PENDING" },
-    });
-
-    return { message: "Document deleted successfully and profile status updated to PENDING" };
   }
 
   // =========================================================

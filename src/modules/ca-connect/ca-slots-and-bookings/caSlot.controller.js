899415986fc17935d8ee365eb442696a42adc9b6
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2026-03-29 15:31:37 +0530

    add booking apis in user and slot booking

--- src/modules/ca-connect/ca-slots-and-bookings/caSlot.controller.js
+++ src/modules/ca-connect/ca-slots-and-bookings/caSlot.controller.js
@@ -54,3 +54,50 @@ exports.createBooking = async (req, res) => {
     });
   }
 };
+
+//here normal user is need to get its latest booking by booking code
+exports.getBookingByCode = async (req, res, next) => {
+  try {
+    const { bookingCode } = req.params;
+
+    const booking = await service.getByBookingCode(
+      bookingCode,
+      req.user
+    );
+
+    res.json({
+      success: true,
+      data: booking
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.listMyBookings = async (req, res, next) => {
+  try {
+    user = req.user;
+    const bookings = await service.listUserBookings(user);
+
+    res.json({
+      success: true,
+      data: bookings
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.listCABookings = async (req, res, next) => {
+  try {
+    const CA = req.user;
+    const bookings = await service.listCABookings(CA);
+
+    res.json({
+      success: true,
+      data: bookings
+    });
+  } catch (err) {
+    next(err);
+  }
+};

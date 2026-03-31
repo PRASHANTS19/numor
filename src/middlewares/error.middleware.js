f6605a01123a781e09cdc0147360617509600f42
Aastik <aastikyadav1997.ay@gmail.com>
2026-03-31 19:27:20 +0530

    Added console logging in error middleware

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -2,7 +2,7 @@ const {appLogger} = require('../utils/logger');
 
 module.exports = (err, req, res, next)=> {
     appLogger.error(err);
-
+    console.log('Error (inside error middleware):', err);
     res.status(err.status || 500).json({
         success: false,
         message: err.message || 'Internal Server Error',
